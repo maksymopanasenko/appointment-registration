1dc586f4edc79293cfded0707c2e8dcd4df1fcb3
Andrew3552 <arbrustrututu66@gmail.com>
2023-09-11 17:14:47 +0300

    card display

--- js/functions/initialRender.js
+++ js/functions/initialRender.js
@@ -1,11 +1,28 @@
 import getCards from "../api/getCards.js";
-            
+import Card from "../classes/cards.js";
+
+
+// В функции initialRender
 async function initialRender() {
     const cards = await getCards();
-    if(cards.length !== 0) {
-        // here should be a render of instances of class
-        document.getElementById('root').innerHTML = '';
+    const rootContainer = document.getElementById('root');
+    rootContainer.innerHTML = '';
+
+    if (cards.length !== 0) {
+        cards.forEach((cardData) => {
+            const cardInstance = new Card(
+                cardData.doctor,
+                cardData.name
+            );
+
+            const cardElement = cardInstance.render();
+
+            rootContainer.appendChild(cardElement);
+        });
+    } else {
+        rootContainer.textContent = 'No items have been added';
     }
 }
+
 
 export default initialRender;
