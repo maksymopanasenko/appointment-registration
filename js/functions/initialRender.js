d8cc23f6ab28ff6ec1c761e5eb7744349bffe1ac
Andrew3552 <arbrustrututu66@gmail.com>
2023-09-12 18:25:29 +0300

    added an urgency filter

--- js/functions/initialRender.js
+++ js/functions/initialRender.js
@@ -1,5 +1,6 @@
 import getCards from "../api/getCards.js";
 import { CardiologistVisit, DentistVisit, TherapistVisit } from "../classes/visits.js";
+import filterCards from "./filter.js";
 
 
 async function initialRender() {
@@ -9,7 +10,7 @@ async function initialRender() {
     rootContainer.innerHTML = '';
 
     if (cards.length !== 0) {
-        cards.forEach(({doctor, purpose, description, urgency, name, pressure, bmi, disease, age, lastVisit}) => {
+        cards.forEach(({ doctor, purpose, description, urgency, name, pressure, bmi, disease, age, lastVisit }) => {
             if (doctor == 'cardiologist') {
                 new CardiologistVisit(doctor, purpose, description, urgency, name, pressure, bmi, disease, age).render()
             } else if (doctor == 'dentist') {
@@ -18,10 +19,17 @@ async function initialRender() {
                 new TherapistVisit(doctor, purpose, description, urgency, name, age).render();
             }
         });
+        filterCards('', '');
     } else {
         rootContainer.textContent = 'No items have been added';
     }
 }
+
+// Вызываем функцию initialRender после загрузки DOM
+document.addEventListener("DOMContentLoaded", function () {
+    initialRender();
+});
+
 
 
 export default initialRender;
