b825a38c159388bf3ea1d52cb89cc93da962c562
Andrew2553 <armbrustrututu66@gmail.com>
2023-09-19 09:53:09 +0300

    filter by status

--- js/functions/filter.js
+++ js/functions/filter.js
@@ -1,4 +1,4 @@
-function filterCards(searchInput, urgencySelect) {
+function filterCards(searchInput, statusSelect, urgencySelect) {
     const cards = document.querySelectorAll(".col-sm-6");
 
     cards.forEach(card => {
@@ -6,15 +6,14 @@ function filterCards(searchInput, urgencySelect) {
         const nameElement = card.querySelector("#name");
         const purposeElement = card.querySelector("#reason");
         const descriptionElement = card.querySelector("#description");
+        const statusElement = card.querySelector(".check-box");
         const urgencyElement = card.querySelector("#urgency");
-
 
         const doctor = doctorElement ? doctorElement.textContent.toLowerCase() : "";
         const name = nameElement ? nameElement.textContent.toLowerCase() : "";
         const purpose = purposeElement ? purposeElement.textContent.toLowerCase() : "";
         const description = descriptionElement ? descriptionElement.textContent.toLowerCase() : "";
         const urgencyText = urgencyElement ? urgencyElement.textContent.toLowerCase() : "";
-     
 
         const containsSearchTerm =
             doctor.includes(searchInput) ||
@@ -22,14 +21,18 @@ function filterCards(searchInput, urgencySelect) {
             purpose.includes(searchInput) ||
             description.includes(searchInput);
 
-            
+        const isStatusElementChecked = statusElement ? statusElement.checked : false;
+        const isStatus = (!statusSelect || statusSelect === "Select the status") || 
+        (statusSelect === "1" && !isStatusElementChecked) ||
+        (statusSelect === "2" && isStatusElementChecked);
+
         const isUrgent =
-            (!urgencySelect || urgencySelect === "Select the urgency") || 
+            (!urgencySelect || urgencySelect === "Select the urgency") ||
             (urgencySelect === "3" && urgencyText.includes("urgent")) ||
             (urgencySelect === "4" && urgencyText.includes("priority")) ||
             (urgencySelect === "5" && urgencyText.includes("regular"));
 
-        if (containsSearchTerm && isUrgent) {
+        if (containsSearchTerm && isStatus && isUrgent) {
             card.style.display = "block";
         } else {
             card.style.display = "none";
